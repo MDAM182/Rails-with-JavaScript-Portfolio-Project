4b4e5ff5e72e7a5cc7923529278984fa28e28ae2
MDAM182 <mdam2345@gmail.com>
2019-08-25 19:17:37 -0400

    modified seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,22 @@ Workout.create(level:'Intermediate')
 Workout.create(level:'Advanced')
 Workout.create(level:'Expert')
 
+User.create(username:"Alfa" , email: "alfa@alfa.com" , password: "army1", admin: false)
+User.create(username:"Bravo" , email:"bravo@bravo.com" , password: "army1", admin: false)
+User.create(username:"Delta" , email:"delta@delta.com" , password: "army1", admin: false)
+User.create(username:"Mike" , email:"mike@mike.com" , password: "army1", admin: true)
+User.create(username:"Echo" , email:"echo@echo.com" , password: "army1", admin: false)
+
+
+
+Category.create(name: "Core")
+Category.create(name: "Upperbody")
+Category.create(name: "Lowerbody")
+
 Workout.create(title: "Push-Ups", description: "A basic push up is an effective
-  way to strengthen the chest and arm muscles and can be easily scaled as you get stronger.")
+  way to strengthen the chest and arm muscles and can be easily scaled as you get stronger.", level: "", user_id: 1, category_id: 2)
 
 Workout.create(title: "Pull-Ups", description: "Pull ups work all of the “pull”
-     muscles in your body: your back, biceps, forearms. They are indicative of your level of fitness.")
+     muscles in your body: your back, biceps, forearms. They are indicative of your level of fitness.", level:"", user_id: 2, category_id: 2)
 
-Workout.create(title: "Sit-Ups", description: "Psit ups can help build your core and abdominal muscles.")
-
-Categorey.create(name: "Core")
-Categorey.create(name: "Upperbody")
-Categorey.create(name: "Lowerbody")
+Workout.create(title: "Sit-Ups", description: "Psit ups can help build your core and abdominal muscles.",level:"", user_id: 3, category_id: 1)
